d3bc0c07bc32d9595b84050d26c14f970433ef58
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-12-14 11:56:00 +0530

    updated

---
+++ src/Service.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Service.css";
+
+function Service() {
+  return (
+    <div className="service component__space">
+      <div className="heading">
+          <h1 className="heading">My Awesome Service</h1>
+          <p className="heading p__color">There are many variations of passages of Lorem Ipsum available,</p>
+          <p className="heading p__color">but the majority have suffered alteration.</p>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col__3">
+            <div className="servie__box pointer">
+              <div className="icon">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Service
