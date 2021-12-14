e3588468d2c2ddc0bc50a34976a1e0c232e8d027
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-12-14 11:10:46 +0530

    Updated

---
+++ src/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Home.css";
+import logo from "./img/logo.png";
+
+function Home() {
+  return (
+    <div className="home" id="Home">
+      <div className="home__bg">
+        <div className="header d__flex align__items__center pxy__30">
+          <a className="logo" href="">
+            <img src={logo} alt="" />
+          </a>
+          <div className="navigation pxy__30">
+            <ul className="navbar d__flex">
+              <a href = "#Home" ><li className="nav__items mx__15">Home</li></a>
+              <a href = "#About"><li className="nav__items mx__15">About</li></a>
+              <a href = "#Services"><li className="nav__items mx__15">Services</li></a>
+              <a href = "#Portfolio"><li className="nav__items mx__15">Portfolio</li></a>
+              <a href = "#Blog"><li className="nav__items mx__15">Blog</li></a>
+              <a href = "#Contact"><li className="nav__items mx__15">Contact</li></a>
+            </ul>
+          </div>
+        </div>
+        {/* HOME CONTENT */}
+        <div className="container">
+          <div className="home__content">
+            <div className="home__metat">
+                <h1 className="home__text pz__10">
+                  WELCOME TO MY WORLD
+                </h1>
+                <h2 className="home__text pz__10">
+                  Hi, I'm Jyotirmoy Roy
+                </h2>
+                <h3 className="home__text sweet pz__10">
+                  FullStack Developer.
+                </h3>
+                <h4 className="home__text pz__10">
+                  based in INDIA.
+                </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
