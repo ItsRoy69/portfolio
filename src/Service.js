0a92abb300c3482eb192d54ad1d766d912969477
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-12-18 21:55:52 +0530

    Update Service.js

--- src/Service.js
+++ src/Service.js
@@ -16,6 +16,9 @@ function Service() {
               <div className="icon">
 
               </div>
+              <div className="service__meta">
+                  <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              </div>
             </div>
           </div>
         </div>
