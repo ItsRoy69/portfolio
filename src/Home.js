f163ec89c680e5e9f41c616341e05586e3a23730
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-12-21 21:58:43 +0530

    Updated

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Home.css";
 import logo from "./img/logo.png";
 
 function Home() {
-  return (
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    header.classList.toggle("active", window.scrollY > 0);
+  });
+
+  // Toogle Menu
+   const [show, setShow] = useState(true);
+
+return (
     <div className="home" id="Home">
       <div className="home__bg">
         <div className="header d__flex align__items__center pxy__30">
-          <a className="logo" href="">
+          <div className="logo">
             <img src={logo} alt="" />
-          </a>
+          </div>
           <div className="navigation pxy__30">
             <ul className="navbar d__flex">
-              <a href = "#Home" ><li className="nav__items mx__15">Home</li></a>
-              <a href = "#About"><li className="nav__items mx__15">About</li></a>
-              <a href = "#Services"><li className="nav__items mx__15">Services</li></a>
-              <a href = "#Portfolio"><li className="nav__items mx__15">Portfolio</li></a>
-              <a href = "#Blog"><li className="nav__items mx__15">Blog</li></a>
-              <a href = "#Contact"><li className="nav__items mx__15">Contact</li></a>
+              <a href="#Home">
+                <li className="nav__items mx__15">Home</li>
+              </a>
+              <a href="#About">
+                <li className="nav__items mx__15">About</li>
+              </a>
+              <a href="#Services">
+                <li className="nav__items mx__15">Services</li>
+              </a>
+              <a href="#Portfolio">
+                <li className="nav__items mx__15">Portfolio</li>
+              </a>
+              <a href="#Blog">
+                <li className="nav__items mx__15">Blog</li>
+              </a>
+              <a href="#Contact">
+                <li className="nav__items mx__15">Contact</li>
+              </a>
             </ul>
           </div>
+        
         </div>
-        {/* HOME CONTENT */}
+
+          {/* HOME CONTENT */}
         <div className="container">
           <div className="home__content">
             <div className="home__metat">
