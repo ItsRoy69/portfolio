debf4a96b19b4022e11852067b05d906a17cf24c
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-12-21 11:05:53 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 import aboutImg from "./img/about.jpg";
-
 function About() {
+  //  Up To Top Btn
+    window.addEventListener("scroll", function(){
+      const upToTop = document.querySelector("a.bottom__to__top");
+       upToTop.classList.toggle("active", window.scrollY > 0)
+    });
   return (
-    <div className="about component__space">
+    <div className="about component__space" id="About">
       <div className="container">
         <div className="row">
           <div className="col__2">
@@ -41,8 +45,26 @@ function About() {
           </div>
         </div>
       </div>
+      {/* UP TO TOP BTN */}
+      <div className="up__to__top__btn">
+        <a href="#" className="bottom__to__top">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-up white"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+          />
+        </svg>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
